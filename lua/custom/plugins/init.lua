dffdde97aae2706b4f94f1bd92ebf32fbe7b02c8
Girik Bajaj <girik.bajaj@leverageedu.com>
2026-02-10 23:33:13 +0530

    add harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,4 +6,36 @@ return {
   { 'christoomey/vim-tmux-navigator' },
   { 'github/copilot.vim' },
   { 'nvim-treesitter/nvim-treesitter-context' },
+  {
+    'ThePrimeagen/harpoon',
+    keys = function()
+      local keys = {
+        {
+          '<leader>H',
+          function()
+            require('harpoon.mark').add_file()
+          end,
+          desc = 'Harpoon Add File',
+        },
+        {
+          '<leader>l',
+          function()
+            require('harpoon.ui').toggle_quick_menu()
+          end,
+          desc = 'Harpoon Quick Menu',
+        },
+      }
+
+      for i = 1, 5 do
+        table.insert(keys, {
+          '<leader>' .. i,
+          function()
+            require('harpoon.ui').nav_file(i)
+          end,
+          desc = 'Harpoon to File ' .. i,
+        })
+      end
+      return keys
+    end,
+  },
 }
